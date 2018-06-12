d9f05dce782dd6dc78ca04f58ea68de21f7f00b3
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-06-12 20:05:04 +0300

    chores: Let prettier make source code prettier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* @flow */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,11 +8,12 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
+                    <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">TMDB Web client</h1>
                 </header>
                 <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
+                    To get started, edit <code>src/App.js</code> and save to
+                    reload.
                 </p>
             </div>
         );
